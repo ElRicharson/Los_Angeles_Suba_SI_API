b338aa7d1389a09d60eee8ea3904926ba031482e
ElRicharson <richarson66@gmail.com>
2022-02-17 16:31:38 -0500

    commit

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-    $dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","");
+    $dbh = new PDO("mysql:host=us-cdbr-east-04.cleardb.com;dbname=schoolstd","bab7b6d6b428e0","5e897f8a");
 ?>
