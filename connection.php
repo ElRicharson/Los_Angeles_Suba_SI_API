9143fb85cead284d3953989ae6d61195c6cde31d
ElRicharson <richarson66@gmail.com>
2022-02-17 16:34:05 -0500

    commit

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-    $dbh = new PDO("mysql:host=us-cdbr-east-04.cleardb.com;dbname=schoolstd","bab7b6d6b428e0","5e897f8a");
+    $dbh = new PDO("mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_73ae08ece5ae102","bab7b6d6b428e0","5e897f8a");
 ?>
